c59f2fa13a4fcdde80b095e314345809f292b663
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-09-30 08:27:41 -0800

    Thread Static

---
+++ CounterClass.cs
@@ -0,0 +1,24 @@
+﻿namespace _002_ThreadsExample
+{
+    internal class CounterClass
+    {
+      //  [ThreadStatic]
+        public static int counter;
+
+        public void Method()
+        {
+            if (counter < 100)
+            {
+                counter++;
+                Console.WriteLine(counter + " Start " + Thread.CurrentThread.GetHashCode());
+
+                var thread = new Thread(Method);
+                thread.Start();
+                
+                thread.Join();
+
+            }
+            Console.WriteLine("Thread {0} finished", Thread.CurrentThread.GetHashCode());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using _002_ThreadsExample;
+
+CounterClass counter = new ();
+Thread thread = new (counter.Method);
+
+thread.Start();
+thread.Join();
+
+Console.WriteLine("Primary thread finished");
+
+//Console.ReadLine();
